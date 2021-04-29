d43ca61223e1628e47abf93aa7c132942c186142
arfaghif <52845090+arfaghif@users.noreply.github.com>
2021-04-29 22:02:23 +0700

    melengkapi tampilan schedule

--- app/src/main/java/com/example/workout/SchedulerDetails.kt
+++ app/src/main/java/com/example/workout/SchedulerDetails.kt
@@ -1,25 +1,26 @@
 package com.example.workout
 
+import android.app.ActionBar
+import android.app.DatePickerDialog
 import android.content.Context
-import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebView
+import android.view.ViewGroup
+
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.example.workout.ui.history.HistoryLogFragment
-import android.app.DatePickerDialog
-import android.widget.DatePicker
+import androidx.core.widget.ImageViewCompat
+import kotlinx.android.synthetic.main.fragment_tracker.*
 import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
+import java.util.*
+
 
 class SchedulerDetails : AppCompatActivity() {
     private val context: Context? = null
     lateinit var nameEditText: EditText
+    lateinit var targetText: EditText
     lateinit var startTimeSchedulePicker : TimePicker
     lateinit var finishTimeSchedulePicker : TimePicker
     lateinit var autoStarCheckBox : CheckBox
@@ -33,10 +34,13 @@ class SchedulerDetails : AppCompatActivity() {
     lateinit var addButton : Button
     lateinit var updateButton : Button
     lateinit var deleteButton : Button
+    lateinit var cyclingButton : ImageButton
+    lateinit var runningButton : ImageButton
     private var repeatValue = 0
     lateinit var button_date: ImageButton
     lateinit var textview_date: TextView
     var cal = Calendar.getInstance()
+    private var isCycling = true
 
 
 
@@ -54,6 +58,9 @@ class SchedulerDetails : AppCompatActivity() {
         addButton = findViewById(R.id.btnAdd)
         updateButton = findViewById(R.id.btnUpdate)
         deleteButton = findViewById(R.id.btnDelete)
+        cyclingButton = findViewById(R.id.cyclingOption)
+        runningButton = findViewById(R.id.runningOption)
+        targetText = findViewById(R.id.target)
         sun = findViewById(R.id.sundayButton)
         mon = findViewById(R.id.mondayButton)
         tue = findViewById(R.id.tuesdayButton)
@@ -67,10 +74,10 @@ class SchedulerDetails : AppCompatActivity() {
         textview_date!!.text = "--/--/----"
 
 
-        val previousTintList = ColorStateList.valueOf(Color.parseColor("#FFFFFFFF"))
+        val previousTintList = ColorStateList.valueOf(Color.parseColor("#FF5A5A5A"))
         val previousTextColor = mon.currentTextColor
         val checkedTintList = ColorStateList.valueOf(Color.parseColor("#FF03DAC5"))
-        val checkedTextColor = Color.WHITE
+        val checkedTextColor = Color.parseColor("#FF03DAC5")
 
 
         val title = this.intent.extras!!.getString("title")
@@ -96,14 +103,27 @@ class SchedulerDetails : AppCompatActivity() {
             finish()
         }
 
+        runningButton.setOnClickListener {
+            ImageViewCompat.setImageTintList(runningButton, checkedTintList)
+            ImageViewCompat.setImageTintList(cyclingButton, previousTintList)
+            isCycling = false
+            targetText.setHint("Steps Target")
+        }
+        cyclingButton.setOnClickListener {
+            ImageViewCompat.setImageTintList(cyclingButton, checkedTintList)
+            ImageViewCompat.setImageTintList(runningButton, previousTintList)
+            isCycling = true
+            targetText.setHint("Km Target")
+        }
+
         sun.setOnClickListener {
             if(repeatValue<1000000){
                 repeatValue += 1000000
-                sun.backgroundTintList = checkedTintList
+//                sun.backgroundTintList = checkedTintList
                 sun.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 1000000
-                sun.backgroundTintList = previousTintList
+//                sun.backgroundTintList = previousTintList
                 sun.setTextColor(previousTextColor)
             }
         }
@@ -111,66 +131,66 @@ class SchedulerDetails : AppCompatActivity() {
        mon.setOnClickListener {
             if(repeatValue%1000000 <100000){
                 repeatValue += 100000
-                mon.backgroundTintList = checkedTintList
+//                mon.backgroundTintList = checkedTintList
                 mon.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 100000
-                mon.backgroundTintList = previousTintList
+//                mon.backgroundTintList = previousTintList
                 mon.setTextColor(previousTextColor)
             }
         }
         tue.setOnClickListener {
             if(repeatValue%100000 <10000){
                 repeatValue += 10000
-                tue.backgroundTintList = checkedTintList
+//                tue.backgroundTintList = checkedTintList
                 tue.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 10000
-                tue.backgroundTintList = previousTintList
+//                tue.backgroundTintList = previousTintList
                 tue.setTextColor(previousTextColor)
             }
         }
         wed.setOnClickListener {
             if(repeatValue%10000 <1000){
                 repeatValue += 1000
-                wed.backgroundTintList = checkedTintList
+//                wed.backgroundTintList = checkedTintList
                 wed.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 1000
-                wed.backgroundTintList = previousTintList
+//                wed.backgroundTintList = previousTintList
                 wed.setTextColor(previousTextColor)
             }
         }
         thu.setOnClickListener {
             if(repeatValue%1000 <100){
                 repeatValue += 100
-                thu.backgroundTintList = checkedTintList
+//                thu.backgroundTintList = checkedTintList
                 thu.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 100
-                thu.backgroundTintList = previousTintList
+//                thu.backgroundTintList = previousTintList
                 thu.setTextColor(previousTextColor)
             }
         }
         fri.setOnClickListener {
             if(repeatValue%100 <10){
                 repeatValue += 10
-                fri.backgroundTintList = checkedTintList
+//                fri.backgroundTintList = checkedTintList
                 fri.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 10
-                fri.backgroundTintList = previousTintList
+//                fri.backgroundTintList = previousTintList
                 fri.setTextColor(previousTextColor)
             }
         }
         sat.setOnClickListener {
             if(repeatValue%10 <1){
                 repeatValue += 1
-                sat.backgroundTintList = checkedTintList
+//                sat.backgroundTintList = checkedTintList
                 sat.setTextColor(checkedTextColor)
             }else{
                 repeatValue -= 1
-                sat.backgroundTintList = previousTintList
+//                sat.backgroundTintList = previousTintList
                 sat.setTextColor(previousTextColor)
             }
         }
@@ -189,12 +209,16 @@ class SchedulerDetails : AppCompatActivity() {
         // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
         button_date.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View) {
-                DatePickerDialog(this@SchedulerDetails,
+                 DatePickerDialog(this@SchedulerDetails,
                         dateSetListener,
                         // set DatePickerDialog to point to today's date when it loads up
                         cal.get(Calendar.YEAR),
                         cal.get(Calendar.MONTH),
-                        cal.get(Calendar.DAY_OF_MONTH)).show()
+                        cal.get(Calendar.DAY_OF_MONTH)).run {
+                            also{datePicker.minDate = cal.timeInMillis}
+                            show()
+                        }
+
             }
 
         })
